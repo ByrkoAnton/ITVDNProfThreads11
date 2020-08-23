8f57b040052895bfadaba27c9257f56c45a10698
Anton Byrko <antonbyrko@gmail.com>
2020-08-24 01:48:20 +0300

    Add project files.

---
+++ strreams11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace strreams11
+{
+    class Program
+    {
+        static readonly object Locker = new object();
+
+        static void Main(string[] args)
+        {
+            Writer writer1 = new Writer 
+                {src = @"D:\test\poem1.txt", dst = @"D:\test\poem1,2.txt"};
+            Thread myThread1 = new Thread(WriteInFile);
+            myThread1.Start(writer1);
+
+            Writer writer2 = new Writer 
+                {src = @"D:\test\poem2.txt", dst = @"D:\test\poem1,2.txt"};
+            Thread myThread2 = new Thread(WriteInFile);
+            myThread2.Start(writer2);
+        }
+
+
+        public static void WriteInFile(object w)
+        {
+            Writer writer = (Writer) w; 
+            lock (Locker)
+            {
+                var fileSrc = new FileInfo(writer.src);
+                FileStream streamRead = fileSrc.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+                byte[] array = new byte[streamRead.Length];
+                streamRead.Read(array, 0, array.Length);
+                streamRead.Close();
+
+                var fileDst = new FileInfo(writer.dst);
+                FileStream streamWrite = fileDst.Open(FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                streamWrite.Write(array);
+                streamWrite.Close();
+            }
+        }
+    }
+   class Writer
+    {
+        public string src;
+        public string dst;
+    }
+}
---
+++ strreamsAdditional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace strreamsAdditional
+{
+    class Program
+    {
+        static readonly object Locker = new object();
+        static int couter = 1;
+        static void Main(string[] args)
+        {
+            
+            Thread thread1 = new Thread(Counter);
+            thread1.Name = "thread1";
+            thread1.Start();
+
+            Thread thread2 = new Thread(Counter);
+            thread2.Name = "thread2";
+            thread2.Start();
+
+            Thread thread3 = new Thread(Counter);
+            thread3.Name = "thread3";
+            thread3.Start();
+
+        }
+        public static void Counter()
+        {
+            lock (Locker)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"{Thread.CurrentThread.Name} --- {couter++}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
